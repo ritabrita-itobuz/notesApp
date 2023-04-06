070daebfc0c09fcf4deab937e0a5887e4dd8f23c
ritabrita_itobuz <ritabrita@itobuz.com>
2023-04-06 15:12:07 +0530

    first commit

---
+++ backend/database.js
@@ -0,0 +1,94 @@
+
+import * as mongoose from 'mongoose';
+import * as Cors from 'cors';
+import express, { request } from 'express';
+
+const app = express();
+
+app.use(express.json());
+// app.use(Cors())
+// app.use(Cors({origin: '*'}));
+
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    // res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    res.setHeader('Access-Control-Allow-Headers','Access-Control-Request-Headers');
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
+    // res.setHeader('Access-Control-Allow-Headers','Origin', 'Content-Type', 'X-Auth-Token');
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");   
+    next();
+})
+// app.use(express.static('/public'));
+
+mongoose.connect("mongodb+srv://ritabrita:rito2001@cluster.qlbdclu.mongodb.net/?retryWrites=true&w=majority", {
+useNewUrlParser: true,
+useUnifiedTopology: true
+}).then(()=>{
+    console.log("Connected...")
+});
+
+const todoSchema = new mongoose.Schema({
+    taskname: {
+        type: String
+    },
+    task: {
+        type: String
+    }
+})
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+app.get("/read", async(req, res) => {
+    const data = await Todo.find({});
+    try {
+        res.send(data);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+app.post("/create", async(req, res) => {
+    const data = new Todo(req.body);
+    console.log(data);
+    try {
+        await data.save();
+        res.send(data);
+      } catch (error) {
+        res.status(500).send(error);
+      }
+})
+
+app.patch("/update/:id", async (req, res) => {
+    try {
+      await Todo.findByIdAndUpdate(req.params.id, req.body);
+      await Todo.save();
+      res.send(data);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+});
+
+app.put("/updatetask/:id", async (req, res) => {
+  try {
+    await Todo.findByIdAndUpdate(req.params.id, req.body);
+    await Todo.save();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+    try {
+      const data = await Todo.findByIdAndDelete(req.params.id);
+  
+      if (!data) res.status(404).send("No item found");
+      res.status(200).send();
+    } catch (error) {
+      res.status(500).send(error);
+    }
+});
+
+app.listen(5501, "127.0.0.1");
---
+++ front-end/script.js
@@ -0,0 +1,88 @@
+
+let display = () => {
+  const url = "http://127.0.0.1:5501/read";
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(data.length);
+      for (let i = 0; i < data.length; i++) {
+        let id = data[i]._id;
+        document.getElementById("addedtasks").innerHTML += 
+        `<ul style="">
+            <li>${data[i].taskname} -- ${data[i].task}
+            <button style="background-color: orange; border-top-left-radius:20%; 
+            border-top-right-radius:20%; border-bottom-left-radius:20%; border-bottom-right-radius:20%;
+            margin-left: 1rem" onclick="remove('${id}')">delete</button>
+            <button style="background-color: orange; border-top-left-radius:20%; 
+            border-top-right-radius:20%; border-bottom-left-radius:20%; border-bottom-right-radius:20%"
+            onclick="modify('${id}')">edit</button>
+            </li>
+        </ul>`;
+      }
+    });
+}
+display();
+
+let add = () => {
+  fetch("http://127.0.0.1:5501/create", {
+    method: "post",
+    body: JSON.stringify({
+      taskname: document.getElementById("entertask").value,
+      task: document.getElementById("describetask").value,
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+  .then((response) => {
+      return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    console.log(data._id);
+    id = data._id;
+    document.getElementById("addedtasks").innerHTML += 
+    `<ul>
+        <li>${document.getElementById("entertask").value} -- ${document.getElementById("describetask").value}
+        <button style="background-color: orange; border-top-left-radius:20%; border-top-right-radius:20%; 
+        border-bottom-left-radius:20%; border-bottom-right-radius:20%; margin-left: 1rem" onclick="remove('${id}')">delete</button>
+        <button style="background-color: orange; border-top-left-radius:20%; 
+        border-top-right-radius:20%; border-bottom-left-radius:20%; border-bottom-right-radius:20%"
+        onclick="modify('${id}')">edit</button>
+        </li>
+    </ul>`;
+  });
+}
+
+let remove = (i) => {
+  fetch(`http://127.0.0.1:5501/delete/${i}`, {
+  method: 'delete',
+  }).then(() => {
+    window.location.reload();
+  })
+}
+
+let modify = (i) => {
+  fetch(`http://127.0.0.1:5501/updatetask/${i}`, {
+  method: 'put',
+  body: JSON.stringify({
+    taskname: document.getElementById("entertask").value,
+    task: document.getElementById("describetask").value,
+  }),
+  headers: {
+    'Content-type': 'application/json',
+  },
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
+}
+
+
+
