bc841c16dd568a54137df50ad8ada2c9e49b0d81
ritabrita_itobuz <ritabrita@itobuz.com>
2023-04-06 15:29:48 +0530

    updated

--- backend/database.js
+++ backend/database.js
@@ -14,7 +14,7 @@ app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Methods", "*");
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE,OPTIONS');
     // res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-    res.setHeader('Access-Control-Allow-Headers','Access-Control-Request-Headers');
+    // res.setHeader('Access-Control-Allow-Headers','Access-Control-Request-Headers');
     // res.setHeader("Access-Control-Allow-Credentials", "true");
     // res.setHeader('Access-Control-Allow-Headers','Origin', 'Content-Type', 'X-Auth-Token');
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");   
