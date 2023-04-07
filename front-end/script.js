b027fd4a567d07c56302e19fa00070d86970a8bc
ritabrita_itobuz <ritabrita@itobuz.com>
2023-04-07 16:49:43 +0530

    designing changed

--- front-end/script.js
+++ front-end/script.js
@@ -12,15 +12,12 @@ let display = () => {
       for (let i = 0; i < data.length; i++) {
         let id = data[i]._id;
         document.getElementById("addedtasks").innerHTML += 
-        `<div style="width: 18rem; height: 7rem; background: #0B65E6;
-            background: -webkit-linear-gradient(top left, #0B65E6, #B5D9DD);
-            background: -moz-linear-gradient(top left, #0B65E6, #B5D9DD);
-            background: linear-gradient(to bottom right, #0B65E6, #B5D9DD);"
+        `<div style="width: 18rem; height: 7rem; background-color: #dd4489b1"
             class="card border border-dark" id="selected">
             <p id="taskname">${data[i].taskname}</p><p id="task">${data[i].task}</p>
             <div class="buttons" style = "margin-top: -4rem">
-              <button class="btn" onclick="showText('${id}')">Edit</button>
-              <button class="btn" style="" onclick="remove('${id}')">Delete</button>
+              <button class="btn edit-button" onclick="showText('${id}')"><img src="./images/edit.png" style="width: 1rem; height: 1rem"></button>
+              <button class="btn delete-button" style="background-color: #7209b5" onclick="remove('${id}')"><img src="./images/delete.png" style="width: 1rem; height: 1.4rem"></button>
             </div>
         </div>`;
       }
@@ -47,15 +44,12 @@ let add = () => {
     console.log(data._id);
     id = data._id;
     document.getElementById("addedtasks").innerHTML += 
-    `<div style="width: 18rem; background: #0B65E6;
-        background: -webkit-linear-gradient(top left, #0B65E6, #B5D9DD);
-        background: -moz-linear-gradient(top left, #0B65E6, #B5D9DD);
-        background: linear-gradient(to bottom right, #0B65E6, #B5D9DD);"
-        class="card border border-dark">
+    `<div style="width: 18rem; background-color: #dd4489b1"
+        class="card border border-dark" id="selected">
         <p id="taskname">${document.getElementById("entertask").value}</p><p id="task">${document.getElementById("describetask").value}</p>
         <div class="buttons" style = "margin-top: -4rem">
-          <button class="btn" onclick="showText('${id}')">Edit</button>
-          <button class="btn" onclick="remove('${id}')">Delete</button>
+          <button class="btn" onclick="showText('${id}')"><img src="./images/edit.png" style="width: 1rem; height: 1rem"></button>
+          <button class="btn" onclick="remove('${id}')" style="background-color: #7209b7">Delete</button>
         </div>
     </div>`;
     document.getElementById("entertask").textContent = "";
