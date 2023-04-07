1bdb8f5874224896a2fdd9bbd13e7cfd649c39db
ritabrita_itobuz <ritabrita@itobuz.com>
2023-04-07 18:52:11 +0530

    changes done

--- front-end/script.js
+++ front-end/script.js
@@ -16,8 +16,12 @@ let display = () => {
             class="card border border-dark" id="selected">
             <p id="taskname">${data[i].taskname}</p><p id="task">${data[i].task}</p>
             <div class="buttons" style = "margin-top: -4rem">
-              <button class="btn edit-button" onclick="showText('${id}')"><img src="./images/edit.png" style="width: 1rem; height: 1rem"></button>
-              <button class="btn delete-button" style="background-color: #7209b5" onclick="remove('${id}')"><img src="./images/delete.png" style="width: 1rem; height: 1.4rem"></button>
+              <button class="btn edit-button" onclick="showText('${id}')">
+                <img src="./images/edit.png" style="width: 1rem; height: 1rem">
+              </button>
+              <button class="btn delete-button" style="background-color: #7209b5" onclick="remove('${id}')">
+                <img src="./images/delete.png" style="width: 1rem; height: 1.4rem">
+              </button>
             </div>
         </div>`;
       }
@@ -48,8 +52,12 @@ let add = () => {
         class="card border border-dark" id="selected">
         <p id="taskname">${document.getElementById("entertask").value}</p><p id="task">${document.getElementById("describetask").value}</p>
         <div class="buttons" style = "margin-top: -4rem">
-          <button class="btn" onclick="showText('${id}')"><img src="./images/edit.png" style="width: 1rem; height: 1rem"></button>
-          <button class="btn" onclick="remove('${id}')" style="background-color: #7209b7">Delete</button>
+          <button class="btn edit-button" onclick="showText('${id}')">
+            <img src="./images/edit.png" style="width: 1rem; height: 1rem">
+          </button>
+          <button class="btn delete-button" style="background-color: #7209b5" onclick="remove('${id}')">
+                <img src="./images/delete.png" style="width: 1rem; height: 1.4rem">
+              </button>
         </div>
     </div>`;
     document.getElementById("entertask").textContent = "";
@@ -80,9 +88,13 @@ let showText = (j) => {
   console.log(j);
   document.getElementById("addedtasks").style.display = "none";
   document.getElementById("edittask").style.display = "";
+  document.getElementById("breaklabel").style.display="none";
+  document.getElementById("addtask").style.display="none";
   document.getElementById("edit").addEventListener('click', () => {
     document.getElementById("addedtasks").style.display = "";
     document.getElementById("edittask").style.display="none";
+    document.getElementById("breaklabel").style.display="";
+    document.getElementById("addtask").style.display="";
     update();
   })
   let update = () => {
