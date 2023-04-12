b98df07fbb7a2278786eaef6249ea0d3aee06aa2
ritabrita_itobuz <ritabrita@itobuz.com>
2023-04-12 09:59:38 +0530

    uploaded

--- backend/database.js
+++ backend/database.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(express.json());
 
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");   
     next();
 })
