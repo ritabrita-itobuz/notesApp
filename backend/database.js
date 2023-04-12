d828eab5b767a792d4e545d74317f0dd3f54972f
ritabrita_itobuz <ritabrita@itobuz.com>
2023-04-12 10:23:08 +0530

    uploaded

--- backend/database.js
+++ backend/database.js
@@ -1,66 +1,76 @@
-
-import * as mongoose from 'mongoose';
-import * as Cors from 'cors';
-import express, { request } from 'express';
+import * as mongoose from "mongoose";
+import * as Cors from "cors";
+import express, { request } from "express";
 
 const app = express();
 
 app.use(express.json());
 
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");   
-    next();
-})
-
-mongoose.connect("mongodb+srv://ritabrita:rito2001@cluster.qlbdclu.mongodb.net/?retryWrites=true&w=majority", {
-useNewUrlParser: true,
-useUnifiedTopology: true
-}).then(()=>{
-    console.log("Connected...")
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  next();
 });
 
-const todoSchema = new mongoose.Schema({
-    taskname: {
-        type: String
-    },
-    task: {
-        type: String
+mongoose
+  .connect(
+    "mongodb+srv://ritabrita:rito2001@cluster.qlbdclu.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     }
-})
+  )
+  .then(() => {
+    console.log("Connected...");
+  });
+
+const todoSchema = new mongoose.Schema({
+  taskname: {
+    type: String,
+  },
+  task: {
+    type: String,
+  },
+});
 
 const Todo = mongoose.model("Todo", todoSchema);
 
-app.get("/read", async(req, res) => {
-    const data = await Todo.find({});
-    try {
-        res.send(data);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+app.get("/read", async (req, res) => {
+  const data = await Todo.find({});
+  try {
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
-app.post("/create", async(req, res) => {
-    const data = new Todo(req.body);
-    console.log(data);
-    try {
-        await data.save();
-        res.send(data);
-      } catch (error) {
-        res.status(500).send(error);
-      }
-})
+app.post("/create", async (req, res) => {
+  const data = new Todo(req.body);
+  console.log(data);
+  try {
+    await data.save();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 app.patch("/update/:id", async (req, res) => {
-    try {
-      await Todo.findByIdAndUpdate(req.params.id, req.body);
-      await Todo.save();
-      res.send(data);
-    } catch (error) {
-      res.status(500).send(error);
-    }
+  try {
+    await Todo.findByIdAndUpdate(req.params.id, req.body);
+    await Todo.save();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.put("/updatetask/:id", async (req, res) => {
@@ -74,14 +84,14 @@ app.put("/updatetask/:id", async (req, res) => {
 });
 
 app.delete("/delete/:id", async (req, res) => {
-    try {
-      const data = await Todo.findByIdAndDelete(req.params.id);
-  
-      if (!data) res.status(404).send("No item found");
-      res.status(200).send();
-    } catch (error) {
-      res.status(500).send(error);
-    }
+  try {
+    const data = await Todo.findByIdAndDelete(req.params.id);
+
+    if (!data) res.status(404).send("No item found");
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.listen(5501, "127.0.0.1");
--- front-end/script.js
+++ front-end/script.js
@@ -3,7 +3,6 @@ document.getElementById("edittask").style.display = "none";
 let fetchedTitle = "";
 
 let display = () => {
-
   const url = "http://127.0.0.1:5501/read";
   fetch(url)
     .then((response) => {
@@ -14,8 +13,9 @@ let display = () => {
       console.log(data.length);
       for (let i = 0; i < data.length; i++) {
         let id = data[i]._id;
-        document.getElementById("addedtasks").innerHTML += 
-        `<div style = "width: 18rem; height: 7rem; background-color: #dd4489b1"
+        document.getElementById(
+          "addedtasks"
+        ).innerHTML += `<div style = "width: 18rem; height: 7rem; background-color: #dd4489b1"
             class = "card border border-dark note" id = "selected">
             <p id = "taskname">${data[i].taskname}</p><p id = "task">${data[i].task}</p>
             <div class = "buttons" style = "margin-top: -4rem">
@@ -29,7 +29,7 @@ let display = () => {
         </div>`;
       }
     });
-}
+};
 display();
 
 let add = () => {
@@ -43,23 +43,27 @@ let add = () => {
       "Content-type": "application/json",
     },
   })
-  .then((response) => {
+    .then((response) => {
       return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    console.log(data._id);
-    id = data._id;
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(data._id);
+      id = data._id;
 
-    document.getElementById("displayNote").innerHTML = 
-    `<section class = "card showNote showAddNote" id = "show">
+      document.getElementById(
+        "displayNote"
+      ).innerHTML = `<section class = "card showNote showAddNote" id = "show">
       <h4>Added Successfully!</h4>
     </section>`;
 
-    document.getElementById("addedtasks").innerHTML += 
-    `<div style = "width: 18rem; margin-top: 1rem; background-color: #dd4489b1"
+      document.getElementById(
+        "addedtasks"
+      ).innerHTML += `<div style = "width: 18rem; margin-top: 1rem; background-color: #dd4489b1"
         class = "card border border-dark" id = "selected">
-        <p id = "taskname">${document.getElementById("entertask").value}</p><p id = "task">${document.getElementById("describetask").value}</p>
+        <p id = "taskname">${
+          document.getElementById("entertask").value
+        }</p><p id = "task">${document.getElementById("describetask").value}</p>
         <div class = "buttons" style = "margin-top: -4rem; margin-bottom: 1.6rem">
           <button class = "btn edit-button" onclick = "showText('${id}')">
             <img src = "./images/edit.png" style = "width: 1rem; height: 1rem">
@@ -69,17 +73,18 @@ let add = () => {
           </button>
         </div>
       </div>`;
-    document.getElementById("entertask").value = "";
-    document.getElementById("describetask").value = "";
-    window.addEventListener('click', () => {
-      document.getElementById("show").style.display = "none";
-    })
-  })
-}
+      document.getElementById("entertask").value = "";
+      document.getElementById("describetask").value = "";
+      window.addEventListener("click", () => {
+        document.getElementById("show").style.display = "none";
+      });
+    });
+};
 
 let remove = (i) => {
-  document.getElementById("displayNote").innerHTML = 
-  `     <section class = "card confirm-box" id="confirm">
+  document.getElementById(
+    "displayNote"
+  ).innerHTML = `     <section class = "card confirm-box" id="confirm">
           <h4>Are you sure to delete it? </h4>
           <img src = "./images/sad.png" style = "">
           <div class = "confirm-buttons">
@@ -88,29 +93,30 @@ let remove = (i) => {
           </div>
         </section>`;
 
-  document.getElementById('confirmDelete').addEventListener('click', () => {
+  document.getElementById("confirmDelete").addEventListener("click", () => {
     fetch(`http://127.0.0.1:5501/delete/${i}`, {
-      method: 'delete',
-      })
+      method: "delete",
+    })
       .then(() => {
         document.getElementById("addedtasks").innerHTML = "";
         display();
       })
       .then(() => {
-        document.getElementById("displayNote").innerHTML = 
-        `<section class="card showNote" id = "show">
+        document.getElementById(
+          "displayNote"
+        ).innerHTML = `<section class="card showNote" id = "show">
           <h4>Deleted Successfully!</h4>
         </section>`;
-        window.addEventListener('click', () => {
+        window.addEventListener("click", () => {
           document.getElementById("show").style.display = "none";
-        })
-      })
-    })
+        });
+      });
+  });
 
-  document.getElementById('confirmCancel').addEventListener('click', () => {
-    document.getElementById('displayNote').innerHTML = "";
-    })
-}
+  document.getElementById("confirmCancel").addEventListener("click", () => {
+    document.getElementById("displayNote").innerHTML = "";
+  });
+};
 
 let showText = (j) => {
   console.log(j);
@@ -119,43 +125,40 @@ let showText = (j) => {
   document.getElementById("breaklabel").style.display = "none";
   document.getElementById("addnote").style.display = "none";
   // document.getElementById("edittitle").value = document.querySelector("#entertask").value;
-  document.getElementById("edit").addEventListener('click', () => {
+  document.getElementById("edit").addEventListener("click", () => {
     document.getElementById("addedtasks").style.display = "";
     document.getElementById("edittask").style.display = "none";
     document.getElementById("breaklabel").style.display = "";
     document.getElementById("addnote").style.display = "";
     update();
-  })
+  });
   let update = () => {
     fetch(`http://127.0.0.1:5501/updatetask/${j}`, {
-      method: 'put',
+      method: "put",
       body: JSON.stringify({
         taskname: document.getElementById("edittitle").value,
         task: document.getElementById("edittext").value,
       }),
       headers: {
-        'Content-type': 'application/json',
+        "Content-type": "application/json",
       },
     })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      document.getElementById("addedtasks").innerHTML = "";
-      display();
-    })
-    .then(() => {
-      document.getElementById("displayNote").innerHTML = 
-      `<section class = "card showNote" id = "show">
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        document.getElementById("addedtasks").innerHTML = "";
+        display();
+      })
+      .then(() => {
+        document.getElementById(
+          "displayNote"
+        ).innerHTML = `<section class = "card showNote" id = "show">
         <h4>Updated Successfully!</h4>
        </section>`;
-      window.addEventListener('click', () => {
-        document.getElementById("show").style.display = "none";
-      })
-    })    
-  }  
-}
-
-
-
-
+        window.addEventListener("click", () => {
+          document.getElementById("show").style.display = "none";
+        });
+      });
+  };
+};
